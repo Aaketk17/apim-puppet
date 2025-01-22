f3b9b392a75ca6ace3cd388dd287ec96c6192c94
Athavan Theivendram <athavan@athavan-2.local>
2025-01-22 11:24:32 +0530

    Changed the hostname

--- modules/apim/manifests/params.pp
+++ modules/apim/manifests/params.pp
@@ -39,7 +39,7 @@ class apim::params inherits apim_common::params {
      This is will become part of the End Point Reference of the
      services deployed on this server instance.
   */
-  $hostname = '15.206.128.170'
+  $hostname = '13.233.224.1'
 
   # ----- api-manager.xml config params -----
   $oauth_configs_revoke_api_url = 'https://localhost:${https.nio.port}/revoke'
