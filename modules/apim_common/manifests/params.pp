76e902068016d91d432135f077c7d9db58a24afe
Athavan Theivendram <athavan@athavan-2.local>
2025-01-30 18:36:33 +0530

    fixed type error in the userdata of nodes

--- modules/apim_common/manifests/params.pp
+++ modules/apim_common/manifests/params.pp
@@ -115,32 +115,23 @@ class apim_common::params {
   ]
 
   if $facts['ec2_metadata']['tags']['instance']['Node'] == 'One' {
-    $event_duplicate_url = 'tcp://localhost:5673'
-    $throttle_decision_endpoints = 'tcp://localhost.com:5672'
-    $throttling_url_group = [
-      {
-        traffic_manager_urls      => 'tcp://localhost.com:9611',
-        traffic_manager_auth_urls => 'ssl://localhost.com:9711'
-      },
-      {
-        traffic_manager_urls      => 'tcp://localhost:9612',
-        traffic_manager_auth_urls => 'ssl://localhost:9712'
-      }
-    ]
+    $event_duplicate_url = 'tcp://apim-node-2.example.com:5673'
+    $throttle_decision_endpoints = 'tcp://apim-node-1.example.com.com:5672'
   } elsif $facts['ec2_metadata']['tags']['instance']['Node'] == 'Two' {
-    $event_duplicate_url = 'tcp://localhost.com:5672'
-    $throttle_decision_endpoints = 'tcp://localhost:5673'
-    $throttling_url_group = [
-      {
-        traffic_manager_urls      => 'tcp://localhost.com:9611',
-        traffic_manager_auth_urls => 'ssl://localhost.com:9711'
-      },
-      {
-        traffic_manager_urls      => 'tcp://localhost:9612',
-        traffic_manager_auth_urls => 'ssl://localhost:9712'
-      }
-    ]
+    $event_duplicate_url = 'tcp://apim-node-1.example.com.com:5672'
+    $throttle_decision_endpoints = 'tcp://apim-node-2.example.com:5673'
   }
+
+  $throttling_url_group = [
+    {
+      traffic_manager_urls      => 'tcp://apim-node-1.example.com:9611',
+      traffic_manager_auth_urls => 'ssl://apim-node-1.example.com:9711'
+    },
+    {
+      traffic_manager_urls      => 'tcp://apim-node-2.example.com:9612',
+      traffic_manager_auth_urls => 'ssl://apim-node-2.example.com:9712'
+    }
+  ]
 
   $gateway_labels = ["Default"]
 
