176dfd7165eeb57635205cc0fe7e90c96f4a430d
Athavan Theivendram <athavan@athavan-2.local>
2025-02-10 19:43:00 +0530

    DB changed

---
+++ modules/apim/files/home/ubuntu/u2-update.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/expect
+spawn /mnt/apim/wso2am-4.2.0/bin/wso2update_linux
+expect "Email:"
+send "athavan@wso2.com\r"
+expect "Password for athavan@wso2.com:"
+send "gabtow-5Wuvky-wemxeq\r"
+interact
