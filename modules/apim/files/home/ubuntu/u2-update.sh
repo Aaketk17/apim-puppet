0de9d03d94b6718657cac9565b29907eb114076b
Athavan Theivendram <athavan@athavan-2.local>
2025-02-11 19:04:05 +0530

    added the encryption

--- modules/apim/files/home/ubuntu/u2-update.sh
+++ modules/apim/files/home/ubuntu/u2-update.sh
@@ -3,7 +3,6 @@
 # Access the environment variables
 # set email $env(EMAIL)
 # set password $env(PASSWORD)
-export JAVA_HOME='/opt/java'
 spawn sh /mnt/apim/wso2am-4.2.0/bin/ciphertool.sh -Dconfigure
 expect "Please Enter Primary KeyStore Password of Carbon Server :"
 send "wso2carbon\r"
