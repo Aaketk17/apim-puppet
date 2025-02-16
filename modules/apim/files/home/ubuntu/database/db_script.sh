4feb594b5b53c237988ec37f99d5a4d9050eb243
Athavan Theivendram <athavan@athavan-2.local>
2025-02-16 10:43:13 +0530

    more changes

--- modules/apim/files/home/ubuntu/database/db_script.sh
+++ modules/apim/files/home/ubuntu/database/db_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mysql -h apim-mysql-db.cgk7myovdx4l.ap-south-1.rds.amazonaws.com -u apimadmin -p < /home/ubuntu/apim/sql_commands_$1.txt
+mysql -h apim-mysql-db.cgk7myovdx4l.ap-south-1.rds.amazonaws.com -u apimadmin -p < /home/ubuntu/database/sql_commands_$1.txt
 echo "DB dscripts updated successfully !!!"
