c4019aefa21851ab2a5ecca4c02950ea483f8679
Athavan Theivendram <athavan@athavan-2.local>
2025-02-17 12:00:21 +0530

    more changes

--- modules/apim/files/home/ubuntu/database/db_script.sh
+++ modules/apim/files/home/ubuntu/database/db_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mysql -h apim-mysql-db.cgk7myovdx4l.ap-south-1.rds.amazonaws.com -u apimadmin -p'kj#$r435%7df' < /home/ubuntu/database/sql_commands_$1.txt
+mysql -h apim-mysql-db.cgk7myovdx4l.ap-south-1.rds.amazonaws.com -u apimadmin -p'kj23rtr4357df' < /home/ubuntu/database/sql_commands_$1.txt
 echo "DB dscripts updated successfully !!!"
 if [ "$2" == "true" ]; then
-    mysql -h apim-mysql-db.cgk7myovdx4l.ap-south-1.rds.amazonaws.com -u apimadmin -p'kj#$r435%7df' < /home/ubuntu/database/table_creation_commands.txt
+    mysql -h apim-mysql-db.cgk7myovdx4l.ap-south-1.rds.amazonaws.com -u apimadmin -p'kj23rtr4357df' < /home/ubuntu/database/table_creation_commands.txt
 else
     echo "Second argument is not 'true' or not provided. Tables creation command will not run"
 fi
