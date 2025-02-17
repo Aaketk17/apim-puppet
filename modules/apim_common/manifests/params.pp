234cba59c04b98dbcef3d45e1d7b14c534f5d113
Athavan Theivendram <athavan@athavan-2.local>
2025-02-17 11:17:01 +0530

    more changes

--- modules/apim_common/manifests/params.pp
+++ modules/apim_common/manifests/params.pp
@@ -148,14 +148,14 @@ class apim_common::params {
   $wso2_rds_host = 'apim-mysql-db.cgk7myovdx4l.ap-south-1.rds.amazonaws.com'
   
   $wso2am_db_url = 'jdbc:mysql://apim-mysql-db.cgk7myovdx4l.ap-south-1.rds.amazonaws.com:3306/apim_db?useSSL=false'
-  $wso2am_db_username = 'apimdbadmin'
-  $wso2am_db_password = 'rterg56jg357df'
+  $wso2am_db_username = 'apimadmin'
+  $wso2am_db_password = 'kj#$r435%7df'
   $wso2am_db_type = 'mysql'
   $wso2am_db_validation_query = 'SELECT 1'
 
   $wso2shared_db_url = 'jdbc:mysql://apim-mysql-db.cgk7myovdx4l.ap-south-1.rds.amazonaws.com:3306/shared_db?useSSL=false'
-  $wso2shared_db_username = 'shareddbadmin'
-  $wso2shared_db_password = 'bnguilui57df'
+  $wso2shared_db_username = 'apimadmin'
+  $wso2shared_db_password = 'kj#$r435%7df'
   $wso2shared_db_type = 'mysql'
   $wso2shared_db_validation_query = 'SELECT 1'
 
